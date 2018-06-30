4ec1f0115915465be5afb06b873d3b9d64f0e86a
llehongseo <culturefor78@gmail.com>
2018-06-30 10:48:58 +0900

    test

---
+++ server.js
@@ -0,0 +1,15 @@
+const express= require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/ang-boot-app'));
+
+app.get('/*',function(req,res,next){
+    console.log(req.method);
+    res.sendFile(path.join(__dirname+'dist/ang-boot-app/index.html'));
+    next();
+});
+var port = process.env.PORT || 3000;
+app.listen(port,"0.0.0.0",function(){
+    console.log("Listening on Port 3000");
+});
